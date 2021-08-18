c8889a7ac3d44ff480479fcec5e134acd3bae7ca
Abdulsamed SAY <abdulsamed8714@gmail.com>
2021-08-18 12:32:55 +0200

    JavaGame updated

--- JavaGame/src/nLayeredDemo/Main.java
+++ JavaGame/src/nLayeredDemo/Main.java
@@ -1,9 +1,23 @@
 package nLayeredDemo;
 
+import nLayeredDemo.business.abstracts.ProductService;
+import nLayeredDemo.business.concretes.ProductManager;
+import nLayeredDemo.core.JLoggerManagerAdapter;
+import nLayeredDemo.dataAccess.concretes.AbcProductDao;
+import nLayeredDemo.entities.concretes.Product;
+
 public class Main {
-	
+
 	public static void main(String[] args) {
-		
+
+		// ProductService productService = new ProductManager(new
+		// HibernateProductDao());
+		ProductService productService = new ProductManager(new AbcProductDao(), new JLoggerManagerAdapter());
+		// This is horrible solution we will change it later! with Hibernate..
+
+		Product product = new Product(3, 1, "Tablet", 1200, 50);
+		productService.add(product);
+
 	}
 
 }
---
+++ JavaGame/src/nLayeredDemo/jLogger/JLoggerManager.java
@@ -0,0 +1,9 @@
+package nLayeredDemo.jLogger;
+
+public class JLoggerManager {
+	
+	public void log(String message) {
+		System.out.println("Logged by jLogger!");
+	}
+
+}
